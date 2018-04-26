f4512f3fa8131c55cba692737d78c82bbcea203f
Algore <actuallyfbi@protonmail.com>
2018-04-25 21:15:01 -0700

    Update test.c

--- test.c
+++ test.c
@@ -1,8 +1,7 @@
 #include <stdio.h> 
-
-int main()
-{
-	printf("im gay"); 
+// updated not autistic version orginal LOLCOW "LMAOCOW"
+int main(){
+	printf("I use shitty brakets -- Lolcow"); 
 	return 0;
 }
 int open(const char *path, int oflag, mode_t mode); 
